36464cd72bc46389bdd164444cf65b4706303d66
Gerrit N. Bouknecht <gbouknecht@server.fake>
2018-12-22 15:21:19 +0100

    Add adding, subtracting and multiplying colors

--- RayTracerChallengeModel/Color.swift
+++ RayTracerChallengeModel/Color.swift
@@ -9,3 +9,37 @@ public struct Color {
         self.blue = blue
     }
 }
+
+// MARK: - Equatable
+
+extension Color: Equatable {
+    public static func ==(lhs: Color, rhs: Color) -> Bool {
+        return equal(lhs.red, rhs.red)
+            && equal(lhs.green, rhs.green)
+            && equal(lhs.blue, rhs.blue)
+    }
+    
+    private static func equal(_ a: Double, _ b: Double) -> Bool {
+        return abs(a - b) < 0.00001
+    }
+}
+
+// MARK: - Operations
+
+extension Color {
+    public static func +(_ a: Color, _ b: Color) -> Color {
+        return Color(a.red + b.red, a.green + b.green, a.blue + b.blue)
+    }
+
+    public static func -(_ a: Color, _ b: Color) -> Color {
+        return Color(a.red - b.red, a.green - b.green, a.blue - b.blue)
+    }
+    
+    public static func *(_ a: Color, _ b: Double) -> Color {
+        return Color(a.red * b, a.green * b, a.blue * b)
+    }
+    
+    public static func *(_ a: Color, _ b: Color) -> Color {
+        return Color(a.red * b.red, a.green * b.green, a.blue * b.blue)
+    }
+}
--- RayTracerChallengeModelTests/ColorTests.swift
+++ RayTracerChallengeModelTests/ColorTests.swift
@@ -8,4 +8,46 @@ class ColorTests: XCTestCase {
         XCTAssertEqual(c.green, 0.4)
         XCTAssertEqual(c.blue, 1.7)
     }
+    
+    func testColorEqualityShouldAllowSlightDifferences() {
+        let c = Color(0.2, 0.3, 0.4)
+        
+        XCTAssertNotEqual(c, Color(0.199990, 0.3, 0.4))
+        XCTAssertEqual   (c, Color(0.199991, 0.3, 0.4))
+        XCTAssertEqual   (c, Color(0.200009, 0.3, 0.4))
+        XCTAssertNotEqual(c, Color(0.200011, 0.3, 0.4))
+        
+        XCTAssertNotEqual(c, Color(0.2, 0.299990, 0.4))
+        XCTAssertEqual   (c, Color(0.2, 0.299991, 0.4))
+        XCTAssertEqual   (c, Color(0.2, 0.300009, 0.4))
+        XCTAssertNotEqual(c, Color(0.2, 0.300010, 0.4))
+        
+        XCTAssertNotEqual(c, Color(0.2, 0.3, 0.399990))
+        XCTAssertEqual   (c, Color(0.2, 0.3, 0.399991))
+        XCTAssertEqual   (c, Color(0.2, 0.3, 0.400009))
+        XCTAssertNotEqual(c, Color(0.2, 0.3, 0.400011))
+    }
+    
+    func testColorsCanBeAdded() {
+        let c1 = Color(0.9, 0.6, 0.75)
+        let c2 = Color(0.7, 0.1, 0.25)
+        XCTAssertEqual(c1 + c2, Color(1.6, 0.7, 1.0))
+    }
+    
+    func testColorsCanBeSubtracted() {
+        let c1 = Color(0.9, 0.6, 0.75)
+        let c2 = Color(0.7, 0.1, 0.25)
+        XCTAssertEqual(c1 - c2, Color(0.2, 0.5, 0.5))
+    }
+    
+    func testColorCanBeMultipliedByScalar() {
+        let c = Color(0.2, 0.3, 0.4)
+        XCTAssertEqual(c * 2, Color(0.4, 0.6, 0.8))
+    }
+    
+    func testColorsCanBeMultiplied() {
+        let c1 = Color(1, 0.2, 0.4)
+        let c2 = Color(0.9, 1, 0.1)
+        XCTAssertEqual(c1 * c2, Color(0.9, 0.2, 0.04))
+    }
 }
