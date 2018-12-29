26282891a6d948ec63a5b8e70d35ca165d095299
Gerrit N. Bouknecht <gbouknecht@server.fake>
2018-12-29 16:38:12 +0100

    Add scaling function

--- RayTracerChallengeModel/Transformations.swift
+++ RayTracerChallengeModel/Transformations.swift
@@ -5,3 +5,11 @@ public func translation(_ x: Double, _ y: Double, _ z: Double) -> Matrix {
         [0 , 0 , 1 , z],
         [0 , 0 , 0 , 1]])
 }
+
+public func scaling(_ x: Double, _ y: Double, _ z: Double) -> Matrix {
+    return Matrix([
+        [x , 0 , 0 , 0],
+        [0 , y , 0 , 0],
+        [0 , 0 , z , 0],
+        [0 , 0 , 0 , 1]])
+}
--- RayTracerChallengeModelTests/TransformationsTests.swift
+++ RayTracerChallengeModelTests/TransformationsTests.swift
@@ -20,4 +20,29 @@ class TransformationsTests: XCTestCase {
         let v = Tuple(fromVector: -3, 4, 5)
         XCTAssertEqual(transform * v, v)
     }
+    
+    func testScalingMovesPointByMultiplication() {
+        let transform = scaling(2, 3, 4)
+        let p = Tuple(fromPoint: -4, 6, 8)
+        XCTAssertEqual(transform * p, Tuple(fromPoint: -8, 18, 32))
+    }
+    
+    func testScalingChangesVectorLength() {
+        let transform = scaling(2, 3, 4)
+        let v = Tuple(fromVector: -4, 6, 8)
+        XCTAssertEqual(transform * v, Tuple(fromVector: -8, 18, 32))
+    }
+    
+    func testInvertedScalingScalesTupleInOppositeWay() {
+        let transform = scaling(2, 3, 4)
+        let inv = transform.inverted()
+        let v = Tuple(fromVector: -4, 6, 8)
+        XCTAssertEqual(inv * v, Tuple(fromVector: -2, 2, 2))
+    }
+    
+    func testScalingCanBeUsedToReflectAcrossAxis() {
+        let transform = scaling(-1, 1, 1)
+        let p = Tuple(fromPoint: 2, 3, 4)
+        XCTAssertEqual(transform * p, Tuple(fromPoint: -2, 3, 4))
+    }
 }
