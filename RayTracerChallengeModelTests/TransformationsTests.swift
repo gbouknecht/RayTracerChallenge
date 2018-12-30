30c4ff3893859857b1af1637e5d26ed3141164b4
Gerrit N. Bouknecht <gbouknecht@server.fake>
2018-12-30 11:17:02 +0100

    Add tests for chained transformations

--- RayTracerChallengeModelTests/TransformationsTests.swift
+++ RayTracerChallengeModelTests/TransformationsTests.swift
@@ -89,4 +89,26 @@ class TransformationsTests: XCTestCase {
         XCTAssertEqual(shearing(xy: 0, xz: 0, yx: 0, yz: 0, zx: 1, zy: 0) * p, point(2, 3, 6))
         XCTAssertEqual(shearing(xy: 0, xz: 0, yx: 0, yz: 0, zx: 0, zy: 1) * p, point(2, 3, 7))
     }
+    
+    func testIndividualTransformationsCanBeAppliedInSequence() {
+        let p = point(1, 0, 1)
+        let a = rotationx(.pi / 2)
+        let b = scaling(5, 5, 5)
+        let c = translation(10, 5, 7)
+        let p2 = a * p
+        let p3 = b * p2
+        let p4 = c * p3
+        XCTAssertEqual(p2, point(1, -1, 0))
+        XCTAssertEqual(p3, point(5, -5, 0))
+        XCTAssertEqual(p4, point(15, 0 , 7))
+    }
+    
+    func testChainedTransformationsMustBeAppliedInReverseOrder() {
+        let p = point(1, 0, 1)
+        let a = rotationx(.pi / 2)
+        let b = scaling(5, 5, 5)
+        let c = translation(10, 5, 7)
+        let t = c * b * a
+        XCTAssertEqual(t * p, point(15, 0, 7))
+    }
 }
