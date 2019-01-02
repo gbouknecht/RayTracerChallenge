b4f71a28aad0db51ee2a0148bb1fed06966f23f2
Gerrit N. Bouknecht <gbouknecht@server.fake>
2019-01-02 11:25:30 +0100

    Change sphere's intersection to support transformations

--- RayTracerChallengeModel/Sphere.swift
+++ RayTracerChallengeModel/Sphere.swift
@@ -3,11 +3,22 @@ import Foundation
 public struct Sphere: Equatable {
     private let id = UUID()
     
+    public let transform: Matrix
+    
+    public init() {
+        transform = identity()
+    }
+    
+    public init(transform: Matrix) {
+        self.transform = transform
+    }
+    
     public func intersect(_ ray: Ray) -> Intersections {
-        let sphereToRay = ray.origin - point(0, 0, 0)    // For now, the sphere is centered at the world origin.
-        let a = ray.direction.dot(ray.direction)
-        let b = 2 * ray.direction.dot(sphereToRay)
-        let c = sphereToRay.dot(sphereToRay) - 1
+        let transformedRay = ray.transformed(transform.inverted())
+        let sphereToTransformedRay = transformedRay.origin - point(0, 0, 0)    // For now, the sphere is centered at the world origin.
+        let a = transformedRay.direction.dot(transformedRay.direction)
+        let b = 2 * transformedRay.direction.dot(sphereToTransformedRay)
+        let c = sphereToTransformedRay.dot(sphereToTransformedRay) - 1
         let discriminant = pow(b, 2) - 4 * a * c
         if discriminant < 0 {
             return Intersections()
--- RayTracerChallengeModelTests/SphereTests.swift
+++ RayTracerChallengeModelTests/SphereTests.swift
@@ -53,4 +53,31 @@ class SphereTests: XCTestCase {
         XCTAssertEqual(xs[0].object as! Sphere, s)
         XCTAssertEqual(xs[1].object as! Sphere, s)
     }
+    
+    func testSphereHasIdentityMatrixAsDefaultTransformation() {
+        let s = Sphere()
+        XCTAssertEqual(s.transform, identity())
+    }
+    
+    func testSpheresTransformationCanBeChanged() {
+        let t = identity().translated(2, 3, 4)
+        let s = Sphere(transform: t)
+        XCTAssertEqual(s.transform, t)
+    }
+    
+    func testScaledSphereCanIntersectRay() {
+        let r = Ray(point(0, 0, -5), vector(0, 0, 1))
+        let s = Sphere(transform: identity().scaled(2, 2, 2))
+        let xs = s.intersect(r)
+        XCTAssertEqual(xs.count, 2)
+        XCTAssertEqual(xs[0].t, 3)
+        XCTAssertEqual(xs[1].t, 7)
+    }
+    
+    func testTranslatedSphereCanIntersectRay() {
+        let r = Ray(point(0, 0, -5), vector(0, 0, 1))
+        let s = Sphere(transform: identity().translated(5, 0, 0))
+        let xs = s.intersect(r)
+        XCTAssertEqual(xs.count, 0)
+    }
 }
