8bb60028c61604a2346964b10e3a251a44f17d85
Gerrit N. Bouknecht <gbouknecht@server.fake>
2019-03-17 09:05:55 +0100

    Add PreparedComputations struct

---
+++ RayTracerChallengeModel/PreparedComputations.swift
@@ -0,0 +1,25 @@
+public struct PreparedComputations {
+    public let t: Double
+    public let object: Object
+    public let point: Tuple
+    public let eyev: Tuple
+    public let normalv: Tuple
+    public let inside: Bool
+
+    public init(intersection: Intersection, ray: Ray) {
+        t = intersection.t
+        object = intersection.object
+        point = ray.position(t)
+        eyev = -ray.direction
+        (normalv, inside) = PreparedComputations.normalvAndInside(object, point, eyev)
+    }
+
+    private static func normalvAndInside(_ object: Object, _ point: Tuple, _ eyev: Tuple) -> (Tuple, Bool) {
+        let normalv = object.normalAt(point)
+        if normalv.dot(eyev) < 0 {
+            return (-normalv, true)
+        } else {
+            return (normalv, false)
+        }
+    }
+}
---
+++ RayTracerChallengeModelTests/PreparedComputationsTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+@testable import RayTracerChallengeModel
+
+class PreparedComputationsTests: XCTestCase {
+    func testPreparedComputationsPrecomputesStateOfIntersection() {
+        let r = Ray(point(0, 0, -5), vector(0, 0, 1))
+        let shape = Sphere()
+        let i = Intersection(4, shape)
+        let comps = PreparedComputations(intersection: i, ray: r)
+        XCTAssertEqual(comps.t, i.t)
+        XCTAssertEqual(comps.object as! Sphere, i.object as! Sphere)
+        XCTAssertEqual(comps.point, point(0, 0, -1))
+        XCTAssertEqual(comps.eyev, vector(0, 0, -1))
+        XCTAssertEqual(comps.normalv, vector(0, 0, -1))
+    }
+
+    func testPreparedComputationsDetectsIfIntersectionOccursOnTheOutside() {
+        let r = Ray(point(0, 0, -5), vector(0, 0, 1))
+        let shape = Sphere()
+        let i = Intersection(4, shape)
+        let comps = PreparedComputations(intersection: i, ray: r)
+        XCTAssertFalse(comps.inside)
+    }
+
+    func testPreparedComputationsDetectsIfIntersectionOccursOnTheInside() {
+        let r = Ray(point(0, 0, 0), vector(0, 0, 1))
+        let shape = Sphere()
+        let i = Intersection(1, shape)
+        let comps = PreparedComputations(intersection: i, ray: r)
+        XCTAssertEqual(comps.point, point(0, 0, 1))
+        XCTAssertEqual(comps.eyev, vector(0, 0, -1))
+        XCTAssertTrue(comps.inside)
+        XCTAssertEqual(comps.normalv, vector(0, 0, -1))
+    }
+}
